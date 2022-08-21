8733948b4175a6dc488a28eb1fe1464f76d54816
zhibei <785740487@qq.com>
2022-08-22 00:02:28 +0800

    添加萌芽背景音乐功能

--- src/main/kotlin/com/github/sword/game/Getfly.kt
+++ src/main/kotlin/com/github/sword/game/Getfly.kt
@@ -3,6 +3,7 @@ package com.github.sword.game
 import com.germ.germplugin.api.GermPacketAPI
 import com.germ.germplugin.api.SoundType.getSoundTypeFromType
 import com.github.sword.sword
+import com.github.sword.sword.config
 import org.bukkit.event.player.PlayerChangedWorldEvent
 import taboolib.common.platform.event.SubscribeEvent
 
@@ -15,19 +16,31 @@ object Getfly {
             val name = player.displayName
             sword.say("§c玩家$name§c飞行已经取消")
         }
-        for (i in sword.config.getStringList("music-world").indices) {
-            if (e.from.name == sword.config.getStringList("music-world").get(i)) {
-                GermPacketAPI.playSound(player,
-                    sword.config.getString("music")?.get(i).toString(),
+    }
+    @SubscribeEvent
+    fun m(e: PlayerChangedWorldEvent) {
+        for (i in config.getStringList("music-world").indices)
+            if (e.from.name == config.getStringList("music-world").get(i)) {
+                GermPacketAPI.playSound(/* player = */
+                    e.player,
+                    /* soundName = */
+                    config.getString("music")?.get(i).toString(),
+                    /* soundType = */
                     getSoundTypeFromType("ambient"),
+                    /* x = */
                     0F,
+                    /* volume = */
                     0F,
+                    /* z = */
                     0F,
+                    /* delayTick = */
                     0,
+                    /* p7 = */
                     1F,
+                    /* pitch = */
                     1F,
+                    /* cycle = */
                     true)
             }
-        }
     }
 }
