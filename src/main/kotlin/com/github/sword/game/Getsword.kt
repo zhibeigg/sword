9a94a98aafb147af8d99735caa68ddd7dfae35d1
zhibei <785740487@qq.com>
2022-08-22 17:40:48 +0800

    改写为利用oa插件

--- src/main/kotlin/com/github/sword/game/Getsword.kt
+++ src/main/kotlin/com/github/sword/game/Getsword.kt
@@ -1,5 +1,8 @@
 package com.github.sword.game
 
+import ac.github.oa.OriginAttribute
+import ac.github.oa.api.OriginAttributeAPI
+import ac.github.oa.api.event.entity.EntityDamageEvent
 import com.github.sword.sword
 import org.bukkit.Bukkit
 import org.bukkit.Material
@@ -10,18 +13,17 @@ import taboolib.platform.util.hasName
 
 object Getsword {
     @SubscribeEvent
-    fun e(attack: EntityDamageByEntityEvent) {
-        if (attack.damager.type == EntityType.PLAYER) {
-            val player = attack.damager.name
+    fun e(attack: EntityDamageEvent) {
+        if (attack.damageMemory.attacker.type == EntityType.PLAYER) {
+            val player = attack.damageMemory.attacker.name
             val inv = Bukkit.getPlayer(player)!!.inventory
             val level = Bukkit.getPlayer(player)!!.level
             if (inv.itemInMainHand.hasName()) {
                 val yl = inv.itemInMainHand.itemMeta!!.displayName
                 if (yl == sword.config.getString("成长型武器名")) {
-                    val damage = attack.finalDamage
-                    attack.damage = level + damage
-                    sword.say("§6纸杯龙剑：§a" + player + "§b使用成长型武器对§c" + attack.entity.name + "§b造成了§4" + attack.finalDamage + "§4伤害")
-                    attack.damager.sendMessage(sword.config.getString("龙剑伤害信息"+attack.finalDamage))
+                    attack.damageMemory.damage.plus(level)
+                    sword.say("§6纸杯龙剑：§a" + player + "§b使用成长型武器对§c" + attack.damageMemory.event.entity.name + "§b造成了§4" + attack.damageMemory.event.damage + "§4伤害")
+                    attack.damageMemory.attacker.sendMessage(sword.config.getString("龙剑伤害信息"+attack.damageMemory.event.damage))
                 }
             }
         }
