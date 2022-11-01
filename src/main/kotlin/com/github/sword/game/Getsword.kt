3a64c56c8d1c83e12bde0364b1f357459c87db07
zhibei <785740487@qq.com>
2022-11-01 18:48:51 +0800

    更新oa

--- src/main/kotlin/com/github/sword/game/Getsword.kt
+++ src/main/kotlin/com/github/sword/game/Getsword.kt
@@ -4,6 +4,7 @@ import ac.github.oa.api.OriginAttributeAPI
 import ac.github.oa.api.event.entity.EntityLoadEquipmentEvent
 import ac.github.oa.internal.core.attribute.AttributeData
 import com.github.sword.sword
+import com.github.sword.sword.debug
 import taboolib.common.platform.event.SubscribeEvent
 import taboolib.module.nms.getName
 import taboolib.platform.util.hasName
@@ -20,18 +21,21 @@ object Getsword {
             if (inv.hasName()) {
                 val name = inv.getName()
                 if (name == sword.config.getString("成长型武器名")) {
-                    player.sendMessage(sword.config.getString("龙剑伤害信息") + player.level)
+                    player.sendMessage(sword.config.getString("龙剑伤害信息") + player.level * sword.config.getInt("xlevel"))
                     val data = AttributeData()
                     data.merge(OriginAttributeAPI.loadList(listOf("体术攻击 +" + player.level * sword.config.getInt("xlevel"))))
                     OriginAttributeAPI.setExtra(uuid, "sword", data)
                     OriginAttributeAPI.callUpdate(player)
+                    debug("已更新call{${player}|${name}}")
                 } else {
                     OriginAttributeAPI.removeExtra(uuid, "sword")
                     OriginAttributeAPI.callUpdate(player)
+                    debug("1已删除 sword 属性")
                 }
             } else {
                 OriginAttributeAPI.removeExtra(uuid, "sword")
                 OriginAttributeAPI.callUpdate(player)
+                debug("2已删除 sword 属性")
             }
         }
     }
